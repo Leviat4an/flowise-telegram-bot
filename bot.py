2016b98da1347b0d65302ad88bc3da772bc005be
Leviat4an <kosyanenkomisha5@gmail.com>
2025-06-09 21:51:16 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,4 +30,4 @@ def main():
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.T_
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
